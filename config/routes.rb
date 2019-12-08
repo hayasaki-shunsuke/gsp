5d4af82dad324825cf898227746c99ef8e6a6796
hayasaki <hayasaki-shunsuke@pepabo.com>
2019-12-08 21:38:33 +0900

    user機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pages/info'
   resources :characters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
