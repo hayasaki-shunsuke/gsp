7ca304a9bcebf6e77477ccae501e1fba40755c78
hayasaki-shunsuke <hayasaki-shunsuke@pepabo.com>
2020-01-25 13:08:00 +0900

    自分の結果のみ表示

--- app/controllers/result_fights_controller.rb
+++ app/controllers/result_fights_controller.rb
@@ -4,7 +4,7 @@ class ResultFightsController < ApplicationController
   # GET /result_fights
   # GET /result_fights.json
   def index
-    @result_fights = ResultFight.all
+    @result_fights = ResultFight.where(user_id: current_user.id)
   end
 
   # GET /result_fights/1
