713b14e9614873dcd951ea15e97da8b5f4372d96
hayasaki <hayasaki-shunsuke@pepabo.com>
2020-04-18 09:43:24 +0900

    グラフのサンプル追加

--- app/javascript/packs/chart.js
+++ app/javascript/packs/chart.js
@@ -1,41 +1,34 @@
 (function() {
   window.draw_graph = function() {
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ["赤", "青", "黄色", "緑", "紫", "橙"],
-        datasets: [{
-          label: '得票数',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
+    var ctx = document.getElementById('myChart');
+
+    var data = {
+      labels: ["1月", "2月", "3月", "4月", "5月"],
+      datasets: [{
+        label: 'マリオ',
+        data: [6800000, 5700000, 4703300, 5203984, 5890874],
+        borderColor: 'rgba(255, 200, 100, 1)',
+        lineTension: 0,
+        fill: false,
+        borderWidth: 3
+      }]
+    };
+
+    var options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            min: 300
+            //beginAtZero: true
+          }
         }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero:true
-            }
-          }]
-        }
       }
+    };
+
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: options
     });
   };
 
