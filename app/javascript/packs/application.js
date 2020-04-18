1e5a510e91be70acaeed158665b689b8a234e267
hayasaki <hayasaki-shunsuke@pepabo.com>
2020-04-18 09:15:40 +0900

    turbolinksを無効化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
-require("turbolinks")
 require("channels")
 require("packs/chart.js")
